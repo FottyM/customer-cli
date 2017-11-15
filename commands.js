455e00a3f1ec77396efb687f7231ee5f8c4b5782
Fortunat Mutunda <fotimutunda@gmail.com>
2017-11-15 20:40:10 +0200

    crud app and link it to npm bash

--- commands.js
+++ commands.js
@@ -1,45 +1,76 @@
+#!/usr/bin/env node
+
 const program = require('commander');
-const { prompt } = require('inquirer');
-const { addCustomer, findCustomer } = require('./index');
+const {prompt} = require('inquirer');
+const {addCustomer, findCustomer, updateCustomer, removeCustomer, listCustomers} = require('./index');
 
 const questions = [
-  {
-    type: 'input',
-    name: 'firstname',
-    message: "Customer's first name"
-  },
-  {
-    type: 'input',
-    name: 'lastname',
-    message: "Customer's last name"
-  },
-  {
-    type: 'input',
-    name: 'phone',
-    message: "Customer's phone"
-  },
-  {
-    type: 'input',
-    name: 'email',
-    message: "Customer's email"
-  }
-]
+    {
+        type: 'input',
+        name: 'firstname',
+        message: "Customer's first name"
+    }, {
+        type: 'input',
+        name: 'lastname',
+        message: "Customer's last name"
+    }, {
+        type: 'input',
+        name: 'phone',
+        message: "Customer's phone"
+    }, {
+        type: 'input',
+        name: 'email',
+        message: "Customer's email"
+    }
+];
 
 program.version('version 0.1.0-beta').description('I am Batman');
 
-// program.command('add <firstname> <lastname> <phone> <email>').alias('a').description('Add customer').action((firstname, lastname, phone, email) => {
-//   addCustomer({firstname, lastname, phone, email});
-// });
 
+
+// Add one customer
 program
-  .command('add')
-  .alias('a')
-  .description('Add customer')
-  .action(() => {
-    prompt( questions ).then( answers => addCustomer( answers ) ).catch( error => console.error(`${error} kept you waiting huh!`))
-  })
+    .command('add')
+    .alias('a')
+    .description('Add customer')
+    .action(() => {
+        prompt(questions)
+            .then(answers => addCustomer(answers))
+            .catch(error => console.error(`${error} kept you waiting huh!`))
+    });
+
+// Find a customer
+program
+    .command('find <name>')
+    .alias('f')
+    .description('Find a customer')
+    .action(name => findCustomer(name));
 
 
+// Update a customer
+program
+    .command('update <_id>')
+    .alias('u')
+    .description('update customer')
+    .action(_id => {
+        prompt(questions)
+            .then(answers => updateCustomer(_id, answers))
+            .catch(error => console.error(error))
+    });
 
-program.command('find <name>').alias('f').description('Find a customer').action(name => findCustomer(name))
+//Delete a customer
+program
+    .command('delete <_id>')
+    .alias('d')
+    .description('delete customer')
+    .action(_id => removeCustomer(_id));
+
+//Find all customers
+program
+    .command('list')
+    .alias('l')
+    .description('Find all customers')
+    .action(() => listCustomers());
+
+
 program.parse(process.argv);
--- index.js
+++ index.js
@@ -4,51 +4,88 @@ var pluralize = require('pluralize')
 mongoose.Promise = global.Promise;
 
 // connect to db
-const db = mongoose.connect('mongodb://fotty:password@ds261745.mlab.com:61745/customer-cli', { useMongoClient: true });
-// mongoose.connection.once('open', function () {
-//     console.log('Connected to DB')
-// }).on('error', function (error) {
-//     console.log(error);
-// });
+const db = mongoose.connect('mongodb://fotty:password@ds261745.mlab.com:61745/customer-cli', {useMongoClient: true});
 
 // Import model
 const Customer = require('./models/customer');
 
 // Add customer
 const addCustomer = (customer) => {
-  Customer.create(customer).then(customer => {
-    console.info('New Customer added');
-    db.close();
-  }).catch( error =>{
-    console.info(error)
-    db.close();
-  });
-}
+    Customer.create(customer).then(customer => {
+        console.info('New Customer added');
+        db.close();
+    }).catch(error => {
+        console.info(error);
+        db.close();
+    });
+};
 
 // Find Customer
-
 const findCustomer = (name) => {
-  // Make case insensitive
-  const search = new RegExp(name, 'i');
-  Customer.find({
-    $or: [
-      {
-        firstname: search
-      }, {
-        lastname: search
-      }
-    ]
-  }).then( customer => {
-    console.info(customer);
-    console.info(`${customer.length} ${pluralize( 'match', customer.length)}`);
-    db.close();
-  }).catch( error => {
-    console.info(error)
-  });
-}
+    // Make case insensitive
+    const search = new RegExp(name, 'i');
+    Customer.find({
+        $or: [
+            {
+                firstname: search
+            }, {
+                lastname: search
+            }
+        ]
+    }).then(customer => {
+        console.info(customer);
+        console.info(`${customer.length} ${pluralize('match', customer.length)}`);
+        db.close();
+    }).catch(error => {
+        console.info(error)
+    });
+};
+
+// Update a customer
+const updateCustomer = (_id, customer) => {
+    Customer.update({_id}, customer)
+        .then(customer => {
+            console.info('Cusotmer has been updated');
+            db.close();
+        }).catch(error => {
+        console.error(error);
+        db.close();
+    });
+};
+
+// Delete a customer
+const removeCustomer = (_id) => {
+    Customer.remove(_id)
+        .then(customer => {
+            console.info('Cusotmer has been removed');
+            db.close();
+        }).catch(error => {
+        console.error(error);
+        db.close();
+    });
+};
+
+// List customer
+
+const listCustomers = () => {
+    Customer.find()
+        .then(customers => {
+            console.info(customers);
+            console.info(`${customers.length} ${pluralize('customer', customers.length)}`);
+            db.close();
+        })
+        .catch(error => {
+            console.error(error);
+            db.close();
+        });
+};
+
 
 // Export All Methods
 module.exports = {
-  addCustomer,
-  findCustomer
-}
+    addCustomer,
+    findCustomer,
+    updateCustomer,
+    removeCustomer,
+    listCustomers
+};
